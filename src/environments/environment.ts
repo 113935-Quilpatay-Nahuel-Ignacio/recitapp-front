fa34fd202ddb7a6ade956263b76f9e0a0f775e72
Nahuel Ignacio Quilpatay - 113935 <113935@tecnicatura.frc.utn.edu.ar>
2025-06-08 14:58:48 -0300

    notifications

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,4 @@
-import { firebaseConfig } from '../app/firebase-config';
-
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
-  firebase: firebaseConfig
+  apiUrl: 'http://localhost:8080/api',
 };
